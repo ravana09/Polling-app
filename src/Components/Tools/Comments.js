2d18a8e274049f19e32aba797fa5dbd1b0c313bd
ravana09 <pradeep09sathish@gmail.com>
2024-07-05 17:35:50 +0530

    "Refactored Comments.js component: reformatted code, added whitespace, and updated indentation; no functional changes."

--- src/Components/Tools/Comments.js
+++ src/Components/Tools/Comments.js
@@ -30,24 +30,32 @@ function Comments() {
     fetchPollData();
     fetchVotedPolls();
     fetchComments();
-  }, []);
+  }, [newComment]);
+
+  //fetch poll data
 
   const fetchPollData = async () => {
     try {
-      const response = await axios.post("http://49.204.232.254:84/polls/getone", {
-        poll_id: pollID,
-      });
+      const response = await axios.post(
+        "http://49.204.232.254:84/polls/getone",
+        {
+          poll_id: pollID,
+        }
+      );
       setFetchData(response.data);
     } catch (error) {
       console.error("Error fetching poll data:", error);
     }
   };
-
+  //fetch voted poll
   const fetchVotedPolls = async () => {
     try {
-      const response = await axios.post("http://49.204.232.254:84/polls/getvoted", {
-        user_id: userId,
-      });
+      const response = await axios.post(
+        "http://49.204.232.254:84/polls/getvoted",
+        {
+          user_id: userId,
+        }
+      );
       const { pollIds } = response.data;
       setVotedPollIds(pollIds);
     } catch (error) {
@@ -55,12 +63,17 @@ function Comments() {
     }
   };
 
+  //fetch comment s
   const fetchComments = async () => {
     try {
-      const response = await axios.post("http://49.204.232.254:84/comment/getbyid", {
-        poll_id: pollID,
-      });
+      const response = await axios.post(
+        "http://49.204.232.254:84/comment/getbyid",
+        {
+          poll_id: pollID,
+        }
+      );
       setShowComments(response.data);
+      console.log(response.data);
     } catch (error) {
       console.error("Error fetching comments:", error);
     }
@@ -71,15 +84,20 @@ function Comments() {
     setPollId(pollId);
   };
 
+  //submit
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (selectedOption && pollId) {
       try {
-        const response = await axios.post("http://49.204.232.254:84/polls/voteonpoll", {
-          poll_id: pollId,
-          user_id: userId,
-          option: selectedOption,
-        });
+        const response = await axios.post(
+          "http://49.204.232.254:84/polls/voteonpoll",
+          {
+            poll_id: pollId,
+            user_id: userId,
+            option: selectedOption,
+          }
+        );
         if (response.status === 200) {
           Swal.fire({
             icon: "success",
@@ -92,12 +110,17 @@ function Comments() {
           });
           const updatedVotedPollIds = [...votedPollIds, pollId];
           setVotedPollIds(updatedVotedPollIds);
-          sessionStorage.setItem("votedPollIds", JSON.stringify(updatedVotedPollIds));
+          sessionStorage.setItem(
+            "votedPollIds",
+            JSON.stringify(updatedVotedPollIds)
+          );
         }
       } catch (error) {
         Swal.fire({
           icon: "error",
-          title: `Error voting: ${error.response ? error.response.data.error : error.message}`,
+          title: `Error voting: ${
+            error.response ? error.response.data.error : error.message
+          }`,
           toast: true,
           position: "top-end",
           showConfirmButton: false,
@@ -108,15 +131,20 @@ function Comments() {
     }
   };
 
+  //comment submit
   const handleCommentSubmit = async (e) => {
     e.preventDefault();
     if (newComment.trim() !== "") {
       try {
-        const response = await axios.post("http://49.204.232.254:84/comment/createcomment", {
-          poll_id: pollID,
-          user_id: userId,
-          comment: newComment,
-        });
+        const response = await axios.post(
+          "http://49.204.232.254:84/comment/createcomment",
+          {
+            poll_id: pollID,
+            user_id: userId,
+            comment: newComment,
+          }
+        );
+
         if (response.status === 200) {
           setNewComment("");
           fetchComments(); // Refresh comments after adding a new one
@@ -129,28 +157,35 @@ function Comments() {
 
   const handleReply = (commentId) => {
     setReplyCommentId(commentId);
+    console.log(commentId, "COmment id");
   };
 
+  //Reply post
   const replyPost = async () => {
-    if(replyComment !== ''){
-    try {
-      const response = await axios.post("http://49.204.232.254:84/comment/replycomment", {
-        poll_id: pollID,
-        user_id: userId,
-        reply_msg: replyComment,
-        comment_id: replyCommentId,
-      });
-      if (response.status === 201) {
-        setReplyCommentId(null);
-        setReplyComment("");
-        fetchComments(); // Refresh comments after adding a reply
+    if (replyComment !== "") {
+      try {
+        const response = await axios.post(
+          "http://49.204.232.254:84/comment/replycomment",
+          {
+            poll_id: pollID,
+            user_id: userId,
+            reply_msg: replyComment,
+            comment_id: replyCommentId,
+          }
+        );
+
+        if (response.status === 201) {
+          setReplyCommentId(null);
+          setReplyComment("");
+          fetchComments(); // Refresh comments after adding a reply
+        }
+      } catch (error) {
+        console.error("Error replying to comment:", error);
       }
-    } catch (error) {
-      console.error("Error replying to comment:", error);
     }
-  }
   };
 
+  //get replies button
   const toggleReplies = (commentId) => {
     setShowReplies({
       ...showReplies,
@@ -158,11 +193,23 @@ function Comments() {
     });
   };
 
+  //reply for reply
+  const handleReplyReply = (commentId) => {
+    console.log(commentId, "Reply for reply ");
+    return <div className="reply-for-reply">hi</div>;
+  };
+
   return (
     <div className="comments-section">
       <Card className="card">
         <div>
-          <Card.Link onClick={() => navigate("/UserDetails", { state: { userID: fetchData.createdBy?._id } })}>
+          <Card.Link
+            onClick={() =>
+              navigate("/UserDetails", {
+                state: { userID: fetchData.createdBy?._id },
+              })
+            }
+          >
             {fetchData.createdBy?.user_name}
           </Card.Link>
         </div>
@@ -201,7 +248,10 @@ function Comments() {
                       </Card.Title>
                     ))}
                   {fetchData._id === pollId && (
-                    <Button type="submit" style={{ margin: 10, backgroundColor: "grey" }}>
+                    <Button
+                      type="submit"
+                      style={{ margin: 10, backgroundColor: "grey" }}
+                    >
                       Vote
                     </Button>
                   )}
@@ -218,10 +268,14 @@ function Comments() {
                 <Form onSubmit={handleCommentSubmit}>
                   <Row>
                     <Col sm={12} md={9} lg={9} xl={9}>
-                      <Form.Group className="mb-3 mt-2" controlId="exampleForm.ControlInput1">
+                      <Form.Group
+                        className="mb-3 mt-2"
+                        controlId="exampleForm.ControlInput1"
+                      >
+                        {/* MAIN comment */}
                         <Form.Control
                           type=""
-                          placeholder="Add a Comment"
+                          placeholder="Add a MAIN Comment"
                           name="Comments"
                           value={newComment}
                           onChange={(e) => setNewComment(e.target.value)}
@@ -229,7 +283,11 @@ function Comments() {
                       </Form.Group>
                     </Col>
                     <Col sm={12} md={3} lg={3} xl={3}>
-                      <Button variant="primary" type="submit" className="mb-3 mt-2">
+                      <Button
+                        variant="primary"
+                        type="submit"
+                        className="mb-3 mt-2"
+                      >
                         Submit
                       </Button>
                     </Col>
@@ -244,39 +302,70 @@ function Comments() {
                           paddingBottom: "10px",
                         }}
                       >
+                        {/* REPLY Comment  */}
                         <p style={{ marginBottom: "5px" }}>
-                          <strong>{comment.user_id.user_name}:</strong> {comment.comment}
-                          <Button onClick={() => handleReply(comment._id)} style={{ marginLeft: "10px" }}>
+                          <strong>{comment.user_id.user_name}:</strong>{" "}
+                          {comment.comment}
+                          <Button
+                            onClick={() => handleReply(comment._id)}
+                            style={{ marginLeft: "10px" }}
+                          >
                             Reply
                           </Button>
-                          <Button href="/" onClick={(e) => { e.preventDefault(); toggleReplies(comment._id); }}>
-                            {showReplies[comment._id] ? "Hide replies" : "Get replies"}
-                          </Button>
+                          <a
+                            href="/"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              toggleReplies(comment._id);
+                            }}
+                          >
+                            {showReplies[comment._id]
+                              ? "Hide replies"
+                              : "Get replies"}
+                          </a>
                         </p>
+
                         {comment._id === replyCommentId && (
                           <div>
                             <Row>
                               <Col sm={9} md={9} lg={9} xl={9}>
-                                <Form.Group className="mb-3 mt-2" controlId="exampleForm.ControlInput1">
+                                <Form.Group
+                                  className="mb-3 mt-2"
+                                  controlId="exampleForm.ControlInput1"
+                                >
                                   <Form.Control
                                     type=""
-                                    placeholder="Add a Comment"
+                                    placeholder="Add a Reply"
                                     name="Comments"
                                     value={replyComment}
-                                    onChange={(e) => setReplyComment(e.target.value)}
+                                    onChange={(e) =>
+                                      setReplyComment(e.target.value)
+                                    }
                                   />
                                 </Form.Group>
                               </Col>
                               <Col sm={3} md={3} lg={3} xl={3}>
-                                <Button variant="primary" type="submit" className="mb-3 mt-2" onClick={replyPost}>
+                                <Button
+                                  variant="primary"
+                                  type="submit"
+                                  className="mb-3 mt-2"
+                                  onClick={replyPost}
+                                >
                                   Reply
                                 </Button>
                               </Col>
                             </Row>
                           </div>
                         )}
+
+                        {/* Reply for Reply comment */}
                         {showReplies[comment._id] && comment.replies && (
-                          <ul style={{ listStyleType: "none", paddingLeft: "20px" }}>
+                          <ul
+                            style={{
+                              listStyleType: "none",
+                              paddingLeft: "20px",
+                            }}
+                          >
                             {comment.replies.map((reply, replyIndex) => (
                               <li
                                 key={replyIndex}
@@ -287,7 +376,25 @@ function Comments() {
                                 }}
                               >
                                 <p style={{ marginBottom: "5px" }}>
-                                  <strong>{reply.user_id.user_name}:</strong> {reply.reply_msg}
+                                  <strong>{reply.user_id.user_name}:</strong>{" "}
+                                  {reply.reply_msg}
+                                  <Button
+                                    onClick={() => handleReplyReply(reply._id)}
+                                    style={{ marginLeft: "10px" }}
+                                  >
+                                    Reply
+                                  </Button>
+                                  <a
+                                    href="/"
+                                    onClick={(e) => {
+                                      e.preventDefault();
+                                      toggleReplies(reply._id);
+                                    }}
+                                  >
+                                    {showReplies[reply._id]
+                                      ? "Hide replies"
+                                      : "Get replies"}
+                                  </a>
                                 </p>
                               </li>
                             ))}
